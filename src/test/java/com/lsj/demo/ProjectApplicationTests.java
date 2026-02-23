868a50f437f317cb7200b6c2dfbec233c85a00f4
leeseonju12 <seonju220@gmail.com>
2026-02-23 09:43:41 +0900

    실행 환경 세팅

---
+++ src/test/java/com/lsj/demo/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
